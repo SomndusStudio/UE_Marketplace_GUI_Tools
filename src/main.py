ca2a676faebea02d66c630dc315bfe3ec75e0cce
SomndusStudio <74624954+SomndusStudio@users.noreply.github.com>
2025-09-02 00:42:50 +0200

    Icon fix and deploy pip cache

--- src/main.py
+++ src/main.py
@@ -8,6 +8,9 @@ from PySide6.QtWidgets import QApplication
 from src.core.version import APP_VERSION, APP_NAME
 from src.gui.main_windows import MainWindow
 
+# important: this registers the compiled resources in Qt
+from src.gui import resources_rc
+
 logging.basicConfig(
     level=logging.INFO,  # or DEBUG for more verbosity
     format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
@@ -20,7 +23,7 @@ def main():
     app = QApplication(sys.argv)
 
     # App icon
-    icon = QIcon("../assets/app.ico")
+    icon = QIcon(":/app.ico")
     app.setWindowIcon(icon)
 
     app.setStyle("Fusion")
